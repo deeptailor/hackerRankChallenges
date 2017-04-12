e535e68bf45a6c5fa92f3f3ff95db6b86526fb0e
Deep Tailor <deeptailor@gmail.com>
2017-04-12 01:11:38 -0700

    insertionSort 1 and 2

--- insertionSort.js
+++ insertionSort.js
@@ -21,38 +21,25 @@ function processData(input) {
 
 // Part 2
 
-var input = "406 157 415 318 472 46 252 187 364 481 450 90 390 35 452 74 196 312 142 160 143 220 483 392 443 488 79 234 68 150 356 496 69 88 177 12 288 120 222 270 441 422 103 321 65 316 448 331 117 183 184 128 323 141 467 31 172 48 95 359 239 209 398 99 440 171 86 233 293 162 121 61 317 52 54 273 30 226 421 64 204 444 418 275 263 108 10 149 497 20 97 136 139 200 266 238 493 22 17 39"
+var input = "406 157 415 318 472 46 252 187 364 481 450 90 390 35 452 74 196 312 142 160 143 220 483 392 443 488 79 234 68 150 356 496 69 88 177 12 288 120 222 270 441 422 103 321 65 316 448 331 117 183 184 128 323 141 467 31 172 48 95 359 239 209 398 99 440 171 86 233 293 162 121 61 317 52 54 273 30 226 421 64 204 444 418 275 263 108 10 149 497 20 97 136 139 200 266 238 493 22 17 1"
 
 
 function insertionSort(input) {
     // var numArray = input.split('\n');
     var inputArray = input.split(' ').map(string => Number(string));
 
-    for(var i = 2; i < inputArray.length; i++){
+    for(var i = 1; i < inputArray.length; i++){
+
         var last = inputArray[i];
         var lastPoint = i - 1;
 
-        while(lastPoint > 0 && inputArray[lastPoint] > last){
+        while(lastPoint > -1 && inputArray[lastPoint] > last){
             inputArray[lastPoint + 1] = inputArray[lastPoint];
             lastPoint--;
         }
 
-        if(lastPoint > -1){
-            inputArray[lastPoint+1] = last;
-        }
+        inputArray[lastPoint+1] = last
     }
-
-    if(inputArray[0] > inputArray[1]){
-      var first = inputArray[0];
-      let index = 1;
-
-      while(index < inputArray.length && inputArray[index] < first){
-        inputArray[index-1] = inputArray[index];
-        index++;
-      }
-      inputArray[index-1] = first;
-    }
-
     return inputArray;
 }
 
